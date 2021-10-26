ff904bdede9fce7b6b66b139fb1e27f7287de744
Ilya Fakhrutdinov <bekamez@gmail.com>
2021-10-26 17:32:28 +0500

    Add proxied url generator

--- src/js/utils.js
+++ src/js/utils.js
@@ -4,12 +4,25 @@ import _ from 'lodash';
 import i18n from 'i18next';
 import onChange from 'on-change';
 
+const PROXY = 'https://hexlet-allorigins.herokuapp.com/get';
+
+const getProxiedUrl = (url) => {
+  const proxy = new URL(PROXY);
+  proxy.searchParams.set('disableCache', 'true');
+  proxy.searchParams.set('url', url);
+
+  return proxy.toString();
+};
+
 const getFeedsUrl = (feeds) => feeds.map((feed) => feed.url);
+
 const getText = (field) => i18n.t(field);
+
 const getPostData = (id, state) => {
   const { posts } = onChange.target(state);
   return _.find(posts, ['id', id]);
 };
+
 const addPostToWatched = (id, watchedState) => {
   const state = watchedState;
   const { watchedPosts } = onChange.target(state).uiState;
@@ -22,6 +35,7 @@ const addPostToWatched = (id, watchedState) => {
 };
 
 export {
+  getProxiedUrl,
   getFeedsUrl,
   getText,
   getPostData,
